82aa5be21acb987ad361c65e029a6cd525fcb118
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2018-03-16 01:44:19 +0100

    Change indentation to 4 and disabled function-paren-newline rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,8 @@
 module.exports = {
     "extends": [ "airbnb-base" ],
     "rules": {
-      "comma-dangle": 0
+      "comma-dangle": 0,
+      "indent": ["error", 4],
+      "function-paren-newline": 0
     }
   };
