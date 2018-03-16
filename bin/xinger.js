8a63e59cefe97c7c4dbff1a8bb10c6f25f186d0a
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2018-03-16 01:46:11 +0100

    Parse cli options and call appropriate function

--- bin/xinger.js
+++ bin/xinger.js
@@ -1,3 +1,19 @@
 #!/usr/bin/env node
 
-console.log('start...')
+const cli = require('cli');
+const R = require('ramda');
+const acceptRequests = require('../src/acceptRequests');
+
+const cliOptions = {
+    accept: ['a', 'Accept all incoming contact requests']
+};
+const optionFunctions = { accept: acceptRequests };
+
+R.pipe(
+    cli.parse,
+    R.filter(R.equals(true)),
+    R.keys,
+    R.flip(R.pickAll)(optionFunctions),
+    R.values,
+    R.map(R.call)
+)(cliOptions);
