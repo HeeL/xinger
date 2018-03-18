530d08e42f2397f43b80ba8e7c6d1eec210c24b5
Sergii Paryzhskyi <sergii.paryzhskyi@holidaycheck.com>
2018-03-19 00:49:42 +0100

    Stub console.log to avoid noise in tests

--- test/unit/src/acceptRequestsSpec.js
+++ test/unit/src/acceptRequestsSpec.js
@@ -11,6 +11,7 @@ const createXingCrawlerStub = (
     visitRequestsPage,
     acceptPendingRequests
 });
+sinon.stub(console, 'log');
 
 test('instantiates injected class', t => {
     const xingCrawlerStub = createXingCrawlerStub();
